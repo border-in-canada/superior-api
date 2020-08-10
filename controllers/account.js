a609eed3926f2671b8d7af6dcc51919f5e6eea3c
David Mathes <mathesd84@gmail.com>
2020-08-09 17:33:33 -0700

    Database connection

--- controllers/account.js
+++ controllers/account.js
@@ -8,7 +8,9 @@ exports.root = (req, res, next) => {
     res.status(200).json({message: 'Successful'});
     next();
 }
+
 exports.getUser = (req, res, next) => {
+    console.log(req);
     const tokens = JSON.stringify(req.body.credentials.accessToken);
     const awsUser = req.body.user.userName;
     const userEmail = req.body.user.email;
